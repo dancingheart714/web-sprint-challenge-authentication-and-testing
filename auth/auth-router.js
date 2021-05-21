9e366afab989c44fda108d65204f8ea786bbde77
Leah King <dancingheart714@yahoo.com>
2021-05-21 09:09:19 -0400

    updated router, server settings and created user-model

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,4 +1,18 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+
+router.post('/register', (req, res) => {
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 8);
+  user.password = hash;
+  Users.add(user)
+    .then((saved) => {
+      res.status(201).json(saved);
+    })
+    .catch((error) => {
+      res.status(500).json(error);
+    });
+});
 
 router.post('/register', (req, res) => {
   res.end('implement register, please!');
